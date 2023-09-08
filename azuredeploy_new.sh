7d72c985fcb2eacc1cbc4ac8cdc4608ba63c29b7
Venura Athukorala <venura9@users.noreply.github.com>
2023-09-08 04:27:02 +0000

    location missing for the new infra

--- azuredeploy_new.sh
+++ azuredeploy_new.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 az login --service-principal -u $ID -p $SECRET --tenant $TENANT
-az group create --name poc
+az group create --name poc --location australiasoutheast
 az deployment group create --template-file azuredeploy_new.json --name azuredeploy_new
 FUNCTIONAPPNAME=$(az deployment group show --name azuredeploy --query properties.outputs.functionAppName.value --output tsv)
 echo $FUNCTIONAPPNAME
