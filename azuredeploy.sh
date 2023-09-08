27d1d47f14541b6793e55918770e70a50e8ac93c
Venura Athukorala <venura9@users.noreply.github.com>
2023-09-08 04:24:36 +0000

    all vars upper case

--- azuredeploy.sh
+++ azuredeploy.sh
@@ -2,7 +2,7 @@
 az login --service-principal -u $ID -p $SECRET --tenant $TENANT
 az group create --name poc --location australiasoutheast
 az deployment group create --template-file azuredeploy.json --name azuredeploy
-functionAppName=$(az deployment group show --name azuredeploy --query properties.outputs.functionAppName.value --output tsv)
-echo $functionAppName
+FUNCTIONAPPNAME=$(az deployment group show --name azuredeploy --query properties.outputs.functionAppName.value --output tsv)
+echo $FUNCTIONAPPNAME
 cd func
-func azure functionapp publish $functionAppName
+func azure functionapp publish $FUNCTIONAPPNAME
--- azuredeploy_new.sh
+++ azuredeploy_new.sh
@@ -2,7 +2,7 @@
 az login --service-principal -u $ID -p $SECRET --tenant $TENANT
 az group create --name poc
 az deployment group create --template-file azuredeploy_new.json --name azuredeploy_new
-functionAppName=$(az deployment group show --name azuredeploy --query properties.outputs.functionAppName.value --output tsv)
-echo $functionAppName
+FUNCTIONAPPNAME=$(az deployment group show --name azuredeploy --query properties.outputs.functionAppName.value --output tsv)
+echo $FUNCTIONAPPNAME
 cd func_new
-func azure functionapp publish $functionAppName --slot staging
+func azure functionapp publish $FUNCTIONAPPNAME --slot staging
